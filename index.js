bdf9bb4af3352ccaf69ecb9779065b4b3b2bc6dc
vlntnwest <152171895+vlntnwest@users.noreply.github.com>
2023-12-19 23:33:04 +0100

    Updates
    JS + CSS

--- index.js
+++ index.js
@@ -5,11 +5,11 @@ function nextPage() {
   function moveButton() {
     var x =
       Math.floor(Math.random() *
-      window.innerWidth - document.getElementById("noButton").offsetWidth);
+      (window.innerWidth - document.getElementById("noButton").offsetWidth));
     var y =
       Math.floor(Math.random() *
-      window.innerHeight -
-        document.getElementById("noButton").offsetHeight);
+      (window.innerHeight -
+        document.getElementById("noButton").offsetHeight));
 
     document.getElementById("noButton").style.left = `${x}px`;
     document.getElementById("noButton").style.top = `${y}px`;
