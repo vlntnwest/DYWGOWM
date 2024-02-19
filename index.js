09ef79c790d5a00c9189fa9b331383fbfb208629
vlntnwest <152171895+vlntnwest@users.noreply.github.com>
2024-02-19 23:11:59 +0100

    Button > div

--- index.js
+++ index.js
@@ -14,3 +14,22 @@ function nextPage() {
     document.getElementById("noButton").style.left = `${x}px`;
     document.getElementById("noButton").style.top = `${y}px`;
   }
+
+   // Fonction pour détecter si l'appareil est un appareil mobile
+   function isMobileDevice() {
+    return (typeof window.orientation !== "undefined") || (navigator.userAgent.indexOf('IEMobile') !== -1);
+  }
+
+   // Fonction pour gérer l'événement en fonction de l'appareil
+   document.addEventListener('DOMContentLoaded', function () {
+    var yesButton = document.getElementById('yesButton');
+    var noButton = document.getElementById('noButton');
+    
+    if (isMobileDevice()) {
+      // Si c'est un appareil mobile, utiliser onclick pour le bouton "No"
+      noButton.onclick = moveButton;
+    } else {
+      // Sinon, utiliser onmouseover pour le bouton "No"
+      noButton.onmouseover = moveButton;
+    }
+  });
